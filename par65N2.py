d5d886fab3b5eae832a4a3b2644534b568575c3a
Endpersu <67841931+Endpersu@users.noreply.github.com>
2024-12-13 12:34:15 +0000

    Ксюша

--- par65N2.py
+++ par65N2.py
@@ -1,22 +1,33 @@
 import random
 
-def average_binary_search_steps():
-    arr = sorted(random.sample(range(101), 32))
-    total_steps = 0
-    for _ in range(1000):
-        target = random.randint(0, 100)
-        left, right = 0, len(arr) - 1
-        steps = 0
-        while left <= right:
-            steps += 1
-            mid = (left + right) // 2
-            if arr[mid] == target:
-                break
-            elif arr[mid] < target:
-                right = mid - 1
-            else:
-                left = mid + 1
-        total_steps += steps
-    return total_steps / 1000
-
-print(average_binary_search_steps())
+def binSearch(arr,target):
+    a=0
+    b=len(arr)-1
+    if target < arr[a] | target > arr[b]:
+        return (-1,0)
+    if target == arr[a]:
+        return (0,1)
+    if target == arr[b]:
+        return (b,1)
+    counter=0
+    while(True):
+        if (b-a)<=1:
+            return (-1,counter)
+        counter+=1
+        c=(a+b)//2
+        if target==arr[c]:
+            return (c,counter)
+        if arr[c]>target:
+            b=c
+        else:
+            a=c
+            
+arr=[] 
+for i in range(33):
+    arr+=[int(100*random.random())]
+            
+a=int(100*random.random())
+print(str(arr))
+print(a)
+res=binSearch(arr,a)
+print(res)
